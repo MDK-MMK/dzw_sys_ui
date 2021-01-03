92e047910fc297b88ab8d59c2478a2da0b2a50ce
joker <1751085399@qq.com>
2021-01-03 20:08:36 +0800

    ljh

--- src/views/ljh/zhiwei.vue
+++ src/views/ljh/zhiwei.vue
@@ -7,7 +7,7 @@
             <el-input
               type="text"
               placeholder="请输入职位名称"
-              v-model="Stars.starts"
+              v-model="Zhiwei.zname"
               class="input-shous"
             >
               <el-button
@@ -37,9 +37,8 @@
           :data="tableData"
           style="width: 100%"
         >
-          <el-table-column prop="starts" label="星级"> </el-table-column>
-          <el-table-column prop="xmoney" label="维修金额"> </el-table-column>
-          <el-table-column prop="sid" label="操作" align="right">
+          <el-table-column prop="zname" label="职位名称"> </el-table-column>
+          <el-table-column prop="zid" label="操作" align="right">
             <template slot-scope="scope">
               <el-button
                 type="warning"
@@ -48,33 +47,13 @@
                 @click="update(scope.row)"
                 >编辑</el-button
               >
-
-              <el-popover placement="top" width="160" v-model="visible">
-                <p>确定删除吗？</p>
-                <div style="text-align: right; margin: 0">
-                  <el-button size="mini" type="text" @click="visible = false"
-                    >取消</el-button
-                  >
-                  <el-button type="primary" size="mini" @click="visible = false,del(scope.row.sid)"
-                    >确定</el-button
-                  >
-                </div>
-                <el-button
-                slot="reference"
+              <el-button
                 type="danger"
                 icon="el-icon-delete"
                 round
+                @click="del(scope.row)"
                 >移除</el-button
               >
-              </el-popover>
-
-              <!-- <el-button
-                type="danger"
-                icon="el-icon-delete"
-                round
-                @click="delete scope.row.sid"
-                >移除</el-button
-              > -->
             </template>
           </el-table-column>
         </el-table>
@@ -99,26 +78,23 @@
       :before-close="handleClose"
     >
       <el-form
-        :model="Starsa"
+        :model="Zhiweia"
         :rules="rules"
-        ref="Starsa"
+        ref="Zhiweia"
         label-width="100px"
         class="demo-ruleForm"
       >
-        <el-form-item label="星级名称" prop="starts">
-          <el-input v-model="Starsa.starts"></el-input>
-        </el-form-item>
-        <el-form-item label="维修金额" prop="xmoney">
-          <el-input v-model="Starsa.xmoney"></el-input>
+        <el-form-item label="职位名称" prop="zname">
+          <el-input v-model="Zhiweia.zname"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button
             type="primary"
-            @click="submitForm('Starsa')"
+            @click="submitForm('Zhiweia')"
             v-loading.fullscreen.lock="fullscreenLoading"
             >立即创建</el-button
           >
-          <el-button @click="resetForm('Starsa')">重置</el-button>
+          <el-button @click="resetForm('Zhiweia')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -129,12 +105,14 @@ export default {
   data() {
     var validatestarts = (rule, value, callback) => {
       if (value === "") {
-        callback(new Error("请输入星级名称！"));
+        callback(new Error("请输入职位名称！"));
       } else {
-        const axios = require("axios");
+        //console.log(this.Zhiweia.zid);
+        if(this.Zhiweia.zid==0){
+           const axios = require("axios");
         let that = this;
         axios
-          .get("http://localhost:8080/dzw_sys/api/Starss/byName/" + value)
+          .get("http://localhost:8080/dzw_sys/api/Zhiwei/byName/" + value)
           .then(function (res) {
             if (res.data != "") {
               callback(new Error("此名称已被占用！"));
@@ -142,12 +120,16 @@ export default {
               callback();
             }
           });
+        }else{
+          callback();
+        }
+       
       }
     };
     var validatexmoney = (rule, value, callback) => {
       const age = /^[0-9]*$/;
       if (value === "") {
-        callback(new Error("请输入星级名称！"));
+        callback(new Error("请输入维修金额！"));
       } else {
         if (!age.test(value)) {
           callback(new Error("金额只能是数字！"));
@@ -157,11 +139,11 @@ export default {
       }
     };
     return {
-        visible:false,
+      visible: false,
       fullscreenLoading: false,
-      Starsa: { starts: "", sid: 0, xmoney: "" },
+      Zhiweia: { zid: 0, zname: "" },
       dialogVisible: false,
-      Stars: { starts: "" }, //条件查询
+      Zhiwei: { zname: "" }, //条件查询
       currentPage: 1, //当前页数
       size: 4, //每页大小
       total: 0, //总条数
@@ -183,14 +165,14 @@ export default {
       let that = this;
       axios
         .post(
-          "http://localhost:8080/dzw_sys/api/Starss/all/" +
+          "http://localhost:8080/dzw_sys/api/Zhiwei/all/" +
             size +
             "/" +
             currentPage,
-          this.Stars
+          this.Zhiwei
         )
         .then(function (res) {
-          //  console.log(res.data);
+            console.log(res.data);
           that.tableData = res.data.list;
           that.total = res.data.total;
         });
@@ -211,6 +193,9 @@ export default {
       this.$confirm("确认关闭？")
         .then((_) => {
           done();
+        this.Zhiweia.zname = "";
+        this.Zhiweia.zid= 0;
+        //console.log(this.Zhiweia);
         })
         .catch((_) => {});
     },
@@ -222,10 +207,12 @@ export default {
           this.fullscreenLoading = true;
           const axios = require("axios");
           let that = this;
-          axios
+          console.log(this.Zhiweia.zid);
+          if(this.Zhiweia.zid===0){
+            axios
             .post(
-              "http://localhost:8080/dzw_sys/api/Starss/insert",
-              this.Starsa
+              "http://localhost:8080/dzw_sys/api/Zhiwei/insert",
+              this.Zhiweia
             )
             .then(function (res) {
               that.fullscreenLoading = false;
@@ -236,43 +223,81 @@ export default {
                   type: "success",
                 });
                 that.dialogVisible = false;
-                that.Starsa.starts = "";
-                that.Starsa.xmoney = "";
+                that.Zhiweia.zname = "";
                 that.selectAll(that.size, that.currentPage);
               } else {
                 that.$message.error("添加失败!");
               }
             });
-        } else {
-          // console.log("error submit!!");
 
+          }else{
+            axios
+            .put(
+              "http://localhost:8080/dzw_sys/api/Zhiwei/update",
+              this.Zhiweia
+            )
+            .then(function (res) {
+              that.fullscreenLoading = false;
+              //console.log(res.data);
+              if (res.data == 1) {
+                that.$message({
+                  message: "修改成功",
+                  type: "success",
+                });
+                that.dialogVisible = false;
+                that.Zhiweia.zname = "";
+                that.Zhiweia.zid =0;
+                that.selectAll(that.size, 1);
+              } else {
+                that.$message.error("修改失败!");
+              }
+            });
+
+
+
+          }
+
+        } else {
           return false;
         }
       });
     },
     //重置
     resetForm(formName) {
-      this.$refs[formName].resetFields();
+      let that=this;
+      if (this.Zhiweia.zid !== 0) {
+        that.Zhiweia.zname = "";
+      }else{
+         this.$refs[formName].resetFields();
+      }
     },
     //删除
-    del(sid) {
+    del(row) {
       this.fullscreenLoading = true;
       const axios = require("axios");
       let that = this;
+      //console.log(row);
       axios
-        .delete("http://localhost:8080/dzw_sys/api/Starss/delete", sid)
-        .then(function () {
+        .delete("http://localhost:8080/dzw_sys/api/Zhiwei/delete/" + row.zid)
+        .then(function (res) {
+          console.log(res.data);
           that.fullscreenLoading = false;
           if (res.data == 1) {
             that.$message({
               message: "移除成功",
               type: "success",
             });
-            that.selectAll(that.size, that.currentPage);
+            that.selectAll(that.size,1);
           } else {
             that.$message.error("添加失败!");
           }
         });
+    },
+
+    update(row) {
+      this.dialogVisible = true;
+      this.Zhiweia.zname = row.zname;
+      this.Zhiweia.zid = row.zid;
     },
   },
 };
