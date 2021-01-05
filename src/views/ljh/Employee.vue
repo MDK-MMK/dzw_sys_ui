474d859defd295eec72b7b62cceba3e3de7bf485
joker <1751085399@qq.com>
2021-01-05 08:43:53 +0800

    ljh

--- src/views/ljh/Employee.vue
+++ src/views/ljh/Employee.vue
@@ -1,39 +1,47 @@
 <template>
   <div>
-    <el-table
+    <div id="bu" style="position: absolute; width: 180px;display:inline-block; border:1px solid #aaa;">
+      <el-table
+      :header-cell-style="{ 'text-align': 'center' }"
+      :cell-style="{ 'text-align': 'center' }"
       :data="tables"
-      style="width: 30%;display"
-      >
-      <el-table-column
-        prop="dename"
-        label="部门"
-        sortable
-        width="180">
-      </el-table-column>
-
-    </el-table>
-
-    <el-table
-      :data="tableData"
-      style="width: 70%"
+      style=""
       :default-sort = "{prop: 'date', order: 'descending'}"
       >
       <el-table-column
-        prop="date"
-        label="日期"
-        sortable
+        prop="dename"
+        label="部门"  
         width="180">
       </el-table-column>
+    </el-table>
+    </div>
+
+    <el-table
+      :data="tableData"
+      style="width: 70%;display:inline-block; margin-left:220px;"
+      :default-sort = "{prop: 'date', order: 'descending'}"
+      >
       <el-table-column
-        prop="name"
+        prop="ename"
         label="姓名"
-        sortable
         width="180">
       </el-table-column>
       <el-table-column
-        prop="address"
-        label="地址"
-        :formatter="formatter">
+        prop="ephone"
+        label="电话"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="etrytimes"
+        label="入职日期"
+        sortable
+        width="180">
+      </el-table-column>
+      
+      <el-table-column
+        prop="elaimes"
+        label="离职时间"
+        sortable>
       </el-table-column>
     </el-table>
   </div>
@@ -45,14 +53,51 @@
   export default {
     data() {
       return {
-        tableData: [],
-        tables:[]
+        tableData: [],//员工数据
+        tables:[],//员工部门
+        currentPage: 1, //当前页数
+        size: 4, //每页大小
+       total: 0, //总条数
+       emp:{ephone :'',ename:'',etrytimes:'',elaimes:'',zid:'',ezuant:'1'},
       }
     },
+    created() {
+     this.selectAlls(6, 1);
+     this.selectBu();
+    },
     methods: {
-      formatter(row, column) {
-        return row.address;
-      }
+      //分页查询员工
+      selectAlls(size, currentPage) {
+        const axios = require("axios");
+        let that = this;
+        axios
+          .post(
+            "http://localhost:8080/dzw_sys/api/Emp/all/" +
+              size +
+              "/" +
+              currentPage,
+            this.emp
+          )
+          .then(function (res) {
+              console.log(res.data);
+            that.tableData = res.data.list;
+            that.total = res.data.total;
+          });
+      },
+      //查询
+      selectBu() {
+        const axios = require("axios");
+        let that = this;
+        axios
+          .post("http://localhost:8080/dzw_sys/api/Emp/bu")
+          .then(function (res) {
+            console.log(res.data);
+            that.tables = res.data;
+          });
+      },
     }
   }
 </script>
+<style scoped>
+
+</style>
