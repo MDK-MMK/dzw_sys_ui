0cc7f87472cd4f39ffaa097747f26ae0d632e4fb
Yang Yu Tao <YangYuTao@998.com>
2021-01-05 14:14:17 +0800

    yyt

--- src/views/yyt/Team.vue
+++ src/views/yyt/Team.vue
@@ -139,10 +139,10 @@
               <el-table-column prop="aname" label="姓名"> </el-table-column>
               <el-table-column prop="asex" label="性别">
                 <template slot-scope="scope">
-                  <p v-if="scope.row.asex==0">男</p>
+                  <p v-if="scope.row.asex == 0">男</p>
                   <p v-else>女</p>
                 </template>
-                 </el-table-column>
+              </el-table-column>
               <el-table-column prop="address" label="地址"> </el-table-column>
               <el-table-column prop="acaid" label="身份证"> </el-table-column>
               <el-table-column prop="acraft" label="维修工种">
@@ -229,7 +229,7 @@
         </el-form-item>
       </el-form>
     </el-dialog>
-
+    <!-- 技工form -->
     <el-dialog
       title="技工"
       :visible.sync="dialogVisibleArtisan"
@@ -245,51 +245,63 @@
         class="demo-form-inline"
       >
         <el-form-item label="手机号码" prop="aphone">
-          <el-input v-model="Artisan.aphone"></el-input>
+          <el-input :disabled="dphone" v-model="Artisan.aphone"></el-input>
         </el-form-item>
         <el-form-item label="技工姓名" prop="aname">
           <el-input v-model="Artisan.aname"></el-input>
         </el-form-item>
         <el-form-item label="星级" prop="sid">
-          <el-select v-model="Artisan.sid" placeholder="请选择" style="width:93%;">
-              <el-option
-                v-for="(item, i) in Starsoptions"
-                :key="i"
-                :label="item.starts"
-                :value="item.sid"
-              >
-              </el-option>
+          <el-select v-model="Artisan.sid" placeholder="请选择">
+            <el-option
+              v-for="(item, i) in Starsoptions"
+              :key="i"
+              :label="item.starts"
+              :value="item.sid"
+            >
+            </el-option>
           </el-select>
         </el-form-item>
-        <!-- <el-form-item label="性别" prop="asex">
-          <el-radio v-model="Artisan.asex" label="0" border >男</el-radio>
-          <el-radio v-model="Artisan.asex" label="1" border>女</el-radio>
-        </el-form-item> -->
 
         <el-form-item label="地址" prop="address">
           <el-input v-model="Artisan.address"></el-input>
         </el-form-item>
-        
-        <el-form-item label="职位" prop="zid">
-          <el-select
-              v-model="Artisan.zid"
-              style="width: 95%"
+
+        <el-form-item label="班组" prop="tid" style="margin-left: -14%">
+          <el-select v-model="Artisan.tid" @change="chaxunzz(Artisan.tid)">
+            <el-option
+              v-for="(item, i) in team"
+              :key="i"
+              :label="item.tname"
+              :value="item.tid"
             >
-              <el-option
-                label="技工组长"
-                value="5"
-              >
-              </el-option>
-              <el-option
-                label="普通技工"
-                value="6"
-              >
-              </el-option>
-            </el-select>
+            </el-option>
+          </el-select>
         </el-form-item>
 
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('Artisan')"
+        <el-form-item label="性别" prop="asex">
+          <el-radio v-model="Artisan.asex" label="0">男</el-radio>
+          <el-radio v-model="Artisan.asex" label="1">女</el-radio>
+        </el-form-item>
+
+        <el-form-item label="职位" prop="zid">
+          <el-select v-model="Artisan.zid">
+            <el-option label="技工组长" value="5" :disabled="zz"> </el-option>
+            <el-option label="普通技工" value="6"> </el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="身份证" prop="acaid">
+          <el-input v-model="Artisan.acaid"></el-input>
+        </el-form-item>
+
+        <el-form-item label="维修工种" prop="acraft" style="padding-left: -16%">
+          <el-input v-model="Artisan.acraft"></el-input>
+        </el-form-item>
+        <el-form-item style="margin-left: 16%">
+          <el-button
+            type="primary"
+            @click="submitForm('Artisan')"
+            v-loading.fullscreen.lock="fullscreenLoading"
             >立即创建</el-button
           >
           <el-button @click="resetForm('Artisan')">重置</el-button>
@@ -301,6 +313,82 @@
 <script>
 export default {
   data() {
+    //维修工种
+    var validateAcraft = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入维修工种！"));
+      } else {
+        callback();
+      }
+    };
+    //技工身份证
+    var validateAcaid = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入技工身份证！"));
+      } else {
+        callback();
+      }
+    };
+    //技工地址
+    var validateAddress = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入技工地址！"));
+      } else {
+        callback();
+      }
+    };
+    //技工姓名
+    var validateAname = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入技工姓名！"));
+      } else {
+        callback();
+      }
+    };
+    //技工手机号码
+    var validateAphone = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入手机号码！"));
+      } else {
+        var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+        if (!myreg.test(value)) {
+          callback(new Error("请输入正确的手机号码！"));
+        } else {
+          if(this.Artisan.arzday!=''){
+            if(this.aaphone===value){
+              callback();
+            }else{
+              const axios = require("axios");
+          let that = this;
+          axios
+            .get("http://localhost:8080/dzw_sys/api/Artisans/ByPhone/" + value)
+            .then(function (res) {
+              //console.log(res.data);
+              if (res.data === "") {
+                callback();
+              } else {
+                callback(new Error("此手机号码已有，请重新输入！"));
+              }
+            });
+            }
+          }else{
+            const axios = require("axios");
+          let that = this;
+          axios
+            .get("http://localhost:8080/dzw_sys/api/Artisans/ByPhone/" + value)
+            .then(function (res) {
+              //console.log(res.data);
+              if (res.data === "") {
+                callback();
+              } else {
+                callback(new Error("此手机号码已有，请重新输入！"));
+              }
+            });
+          }
+          
+        }
+      }
+    };
     //新增班组
     var validateTnaemI = (rule, value, callback) => {
       //console.log(value);
@@ -371,23 +459,26 @@ export default {
       dialogVisibleArtisan: false, //新增技工
       Artisan: {
         aphone: "",
-        sid: "",
-        zid: "",
-        tid: "",
+        sid: 1,
+        zid: "6",
+        tid: 1,
         aname: "",
-        asex: '0',
+        asex: "0",
         address: "",
         acaid: "",
         acraft: "",
         arzday: "",
       }, //技工
       rules: {
-        // aphone: [{ validator: validateAphone, trigger: "blur" }],
-        // aname: [{ validator: validateAname, trigger: "blur" }],
-        // address: [{ validator: validateAddress, trigger: "blur" }],
-        // acaid: [{ validator: validateAcaid, trigger: "blur" }],
-        // acraft: [{ validator: validateAcraft, trigger: "blur" }],
+        aphone: [{ validator: validateAphone, trigger: "blur" }],
+        aname: [{ validator: validateAname, trigger: "blur" }],
+        address: [{ validator: validateAddress, trigger: "blur" }],
+        acaid: [{ validator: validateAcaid, trigger: "blur" }],
+        acraft: [{ validator: validateAcraft, trigger: "blur" }],
       },
+      zz: true,
+      aaphone:'',
+      dphone:false,
     };
   },
   created() {
@@ -397,16 +488,131 @@ export default {
     this.selectZhiWeiAll();
   },
   methods: {
+    //修改技工点击
+    updateA(row) {
+      this.dphone=true;
+      this.Artisan.aphone = row.aphone;
+      this.aaphone=row.aphone;
+      this.Artisan.sid = row.sid;
+      this.Artisan.zid = row.zid;
+      this.Artisan.tid = row.tid;
+      this.Artisan.aname = row.aname;
+      this.Artisan.asex = row.asex+'';
+      this.Artisan.address = row.address;
+      this.Artisan.acaid = row.acaid;
+      this.Artisan.acraft = row.acraft;
+      this.Artisan.arzday = row.arzday;
+      this.dialogVisibleArtisan=true;
+
+    },
+    //技工删除
+    delA(row) {
+      //console.log(row);
+      const axios = require("axios");
+      let that = this;
+      axios
+        .delete(
+          "http://localhost:8080/dzw_sys/api/Artisans/delete/" + row.aphone
+        )
+        .then(function (res) {
+          console.log(res.data);
+          if (res.data == 1) {
+            that.$message({
+              message: "移除成功",
+              type: "success",
+            });
+            that.resetForm("Artisan");
+            that.selectArtisanAll(that.size, 1);
+          } else if(res.data==0) {
+            that.$message.error("移除失败!");
+          }else{
+            that.$message.error("当前技工正在维修!");
+          }
+        });
+    },
+    //查询当前班组是否有组长
+    chaxunzz(tid) {
+      if (tid != 0) {
+        const axios = require("axios");
+        let that = this;
+        axios
+          .get("http://localhost:8080/dzw_sys/api/Artisans/ByZid/" + tid)
+          .then(function (res) {
+            console.log(res.data);
+            if (res.data != 0) {
+              that.zz = true;
+              that.Artisan.zid = "6";
+            } else {
+              that.zz = false;
+            }
+          });
+      }
+    },
+    //提交技工
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert("submit!");
+          if(this.Artisan.arzday!=''){
+            //修改
+            const axios = require("axios");
+          let that = this;
+          this.fullscreenLoading = true;
+          axios
+            .put(
+              "http://localhost:8080/dzw_sys/api/Artisans/update",
+              this.Artisan
+            )
+            .then(function (res) {
+              //console.log(res.data);
+              if (res.data == 1) {
+                that.$message({
+                  message: "编辑成功",
+                  type: "success",
+                });
+                that.dialogVisibleArtisan = false;
+                that.resetForm("Artisan");
+                that.selectArtisanAll(that.size, 1);
+              } else {
+                that.$message.error("编辑失败!");
+              }
+              that.fullscreenLoading = false;
+            });
+
+
+          }else{
+
+            //新增
+            const axios = require("axios");
+          let that = this;
+          this.fullscreenLoading = true;
+          axios
+            .post(
+              "http://localhost:8080/dzw_sys/api/Artisans/insert",
+              this.Artisan
+            )
+            .then(function (res) {
+              //console.log(res.data);
+              if (res.data == 1) {
+                that.$message({
+                  message: "添加成功",
+                  type: "success",
+                });
+                that.dialogVisibleArtisan = false;
+                that.resetForm("Artisan");
+                that.selectArtisanAll(that.size, 1);
+              } else {
+                that.$message.error("添加失败!");
+              }
+              that.fullscreenLoading = false;
+            });
+
+          }
         } else {
-          console.log("error submit!!");
           return false;
         }
       });
     },
+    //重置技工
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
@@ -415,6 +621,7 @@ export default {
       this.$confirm("确认关闭？")
         .then((_) => {
           done();
+          this.resetForm('Artisan');
         })
         .catch((_) => {});
     },
@@ -443,6 +650,20 @@ export default {
     },
     //删除班组
     delT() {
+       if (this.teama.tid == 0) {
+        this.$message({
+          message: "请先选中需要移除的班组!",
+          type: "warning",
+        });
+        return;
+      }
+      if(this.tableData.length!=0){
+        this.$message({
+          message: '当前班组，已有技工不可移除',
+          type: 'warning'
+        });
+        return;
+      }
       const axios = require("axios");
       let that = this;
       //this.fullscreenLoading=true;
@@ -461,7 +682,7 @@ export default {
             that.teama.tid = "";
             that.selectTeamAll();
             that.artisan.tid = 0;
-            that.selectArtisanAll(1, this.currentPage);
+            that.selectArtisanAll(that.size, 1);
           } else {
             that.$message.error("移除失败!");
           }
@@ -599,6 +820,5 @@ export default {
 <style scoped>
 .lilist {
   margin: 5px 0px 5px 0px;
-  
 }
 </style>
