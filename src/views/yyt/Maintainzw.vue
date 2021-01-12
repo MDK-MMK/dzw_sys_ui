958e26ac6ee7ec5a5e56621f59fa2a1b9890624c
Yang Yu Tao <YangYuTao@998.com>
2021-01-12 11:00:24 +0800

    yyt

--- src/views/yyt/Maintainzw.vue
+++ src/views/yyt/Maintainzw.vue
@@ -2,6 +2,11 @@
   <el-row :gutter="12">
     <el-col :span="24">
       <el-card shadow="always">
+        <el-page-header @back="goBack" content="添加维修页面"> </el-page-header>
+        <el-radio-group v-model="radio">
+          <el-radio-button label="站内维修"></el-radio-button>
+          <el-radio-button label="站外维修"></el-radio-button>
+        </el-radio-group>
         <el-form
           :model="inststion"
           status-icon
@@ -24,16 +29,16 @@
             <el-form-item label="救援车辆" prop="wid">
               <el-select v-model="inststion.wid" placeholder="请选择">
                 <el-option
-                  v-for="item in options"
+                  v-for="item in workcaroptions"
                   :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
+                  :label="item.caid"
+                  :value="item.wid"
                 >
                 </el-option>
               </el-select>
             </el-form-item>
           </el-col>
-          <el-col :span="8">
+          <el-col v-show="radio == '站外维修'" :span="8">
             <el-form-item label="接车地址">
               <el-input
                 style="width: 220px"
@@ -50,7 +55,7 @@
             </el-form-item>
           </el-col>
 
-          <el-col :span="24" class="dt">
+          <el-col :span="24" v-show="radio == '站外维修'" class="dt">
             <el-row :gutter="12">
               <el-col :span="24">
                 <el-card shadow="hover" id="container"></el-card>
@@ -62,10 +67,36 @@
             <el-form-item label="维修班组" prop="tid">
               <el-select v-model="inststion.tid" placeholder="请选择">
                 <el-option
-                  v-for="item in options"
+                  v-for="item in teamoptions"
                   :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
+                  :label="item.tname"
+                  :value="item.tid"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="客户姓名" prop="cno">
+              <el-select @change="car(xm)" v-model="xm" placeholder="请选择">
+                <el-option
+                  v-for="item in clioptions"
+                  :key="item.value"
+                  :label="item.cliname"
+                  :value="item.cliphone"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="客户车牌号" prop="cno">
+              <el-select v-model="inststion.cno" placeholder="请选择">
+                <el-option
+                  v-for="item in caroptions"
+                  :key="item.value"
+                  :label="item.cno"
+                  :value="item.cno"
                 >
                 </el-option>
               </el-select>
@@ -84,29 +115,6 @@
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item label="客户车牌号" prop="cno">
-              <el-select v-model="inststion.cno" placeholder="请选择">
-                <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                >
-                </el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-
-          <el-col :span="8">
-            <el-form-item label="里程数" prop="lc">
-              <el-input
-                disabled
-                v-model="inststion.lc"
-                autocomplete="off"
-              ></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
             <el-form-item label="接车时间" prop="jcday">
               <el-date-picker
                 v-model="inststion.jcday"
@@ -119,36 +127,57 @@
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item label="维修备注" prop="insevent">
+            <el-form-item style="width: 100%" label="维修备注" prop="insevent">
               <el-input
+                style="width: 100%"
                 type="textarea"
                 placeholder="请输入内容"
                 v-model="inststion.insevent"
+                :rows="2"
                 maxlength="100"
                 show-word-limit
               >
               </el-input>
             </el-form-item>
           </el-col>
+          <el-col :span="5">
+            <el-form-item
+              v-show="radio == '站外维修'"
+              label="里程数:"
+              prop="lc"
+              style="color: red"
+            >
+              {{ inststion.lc }} (公里)
+            </el-form-item>
+          </el-col>
 
-          <el-table :data="tableData1" style="width: 100%">
-            <el-table-column label="编号" type="index" width="100px">
-            </el-table-column>
+          <el-table :data="tableData" style="width: 100%">
+            <el-table-column label="商品编号" prop="shopid"> </el-table-column>
             <el-table-column label="维修项目名" prop="shopname">
             </el-table-column>
             <el-table-column label="维修价格" prop="sellingprice">
             </el-table-column>
-            <el-table-column label="技工耗时" prop="quantity">
+            <el-table-column v-if="false" label="qq" prop="image">
+            </el-table-column>
+            <el-table-column label="使用数量" prop="quantity">
               <template slot-scope="scope">
-                {{ scope.row.quantity }}/h
+                <el-input-number
+                  v-show="scope.row.quantity!=0"
+                  v-model="scope.row.image"
+                  :min="1"
+                  :max="scope.row.quantity"
+                  label="描述文字"
+                ></el-input-number>
               </template>
             </el-table-column>
             <el-table-column align="right">
               <template slot="header">
-                <el-button type="success" plain>添加维修项目</el-button>
+                <el-button type="success" plain @click="dialogVisible1 = true"
+                  >添加保养项目</el-button
+                >
               </template>
-              <template>
-                <el-button type="danger" plain>移除维修项目</el-button>
+              <template slot-scope="scope">
+                <el-button type="danger" @click="byyc(scope.$index,scope.row)" plain>移除</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -177,7 +206,7 @@
                 <el-button type="success" plain>添加维修材料</el-button>
               </template>
               <template>
-                <el-button type="danger" plain>移除维修材料</el-button>
+                <el-button type="danger" plain>移除</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -186,9 +215,58 @@
             <el-button type="primary" @click="submitForm('inststion')"
               >提交</el-button
             >
-            <el-button @click="resetForm('inststion')">重置</el-button>
           </el-form-item>
         </el-form>
+
+        <el-dialog
+          title="维修项目"
+          :visible.sync="dialogVisible1"
+          width="50%"
+          :before-close="handleClose"
+        >
+          <el-table
+            :data="bytableData"
+            style="width: 100%"
+            @selection-change="handleSelectionChange"
+            :row-key="getRowKey"
+            ref="dataTable"
+          >
+            <el-table-column label="商品编号" prop="shopid"> </el-table-column>
+            <el-table-column label="维修项目名" prop="shopname">
+            </el-table-column>
+            <el-table-column label="维修价格" prop="sellingprice">
+            </el-table-column>
+            <el-table-column label="数量" prop="quantity">
+              <template slot-scope="scope">
+                <p v-if="scope.row.quantity == -1">无</p>
+                <p v-else>{{ scope.row.quantity }}</p>
+              </template>
+            </el-table-column>
+            <el-table-column
+              type="selection"
+              :reserve-selection="true"
+              width="55"
+            >
+            </el-table-column>
+          </el-table>
+          <el-pagination
+            @size-change="handleSizeChange1"
+            @current-change="handleCurrentChange1"
+            :current-page="currentPage1"
+            :page-sizes="[4, 6, 8, 10]"
+            :page-size="size1"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total1"
+          >
+          </el-pagination>
+          <span slot="footer" class="dialog-footer">
+            <el-badge :value="bynum" class="item">
+              <el-button type="primary" plain @click="byqr"
+                >确认</el-button
+              >
+            </el-badge>
+          </span>
+        </el-dialog>
       </el-card>
     </el-col>
   </el-row>
@@ -196,6 +274,7 @@
 
 <script>
 //import BMap from "BMap";
+const axios = require("axios");
 export default {
   data() {
     return {
@@ -204,6 +283,7 @@ export default {
       localSearch: "",
       inststion: {
         inid: 0,
+        wid: 0,
         tid: 0,
         cno: "",
         izt: 0,
@@ -214,13 +294,164 @@ export default {
         lc: "",
         insevent: "",
       },
+      workcar: { caid: "", czt: "0" }, //救援车辆
       rules: {},
+      tableData1: [], //维修材料
+      teamoptions: [], //班组
+      workcaroptions: [], //救援车辆
+      clioptions: [], //用户
+      caroptions: [], //客户车辆
+      radio: "站外维修",
+      xm: "", //客户姓名
+      dialogVisible2: false, //维修材料
+
+      //保养
+      tableData: [], //保养项目
+      dialogVisible1: false, //保养项目
+      bytableData: [], //选择保养项目
+      currentPage1: 1, //当前页数   保养
+      size1: 4, //每页大小          保养
+      total1: 0, //总条数           保养
+      bynum: 0, //保养  确认数量
+      bytable:[],
     };
   },
   mounted() {
-    this.bddt();
+    this.bddt(); //地图
+    this.chaxunTeam(); //班组
+    this.chaxunWorkcar(); //救援车辆
+    this.chaxunweix(); //查询维修项目
+    this.chaxunbaoyang(4, 1); //查询保养项目
+    this.chaxunclient(); //查询用户
   },
   methods: {
+
+     getRowKey (row) {
+      return row.shopid;
+    },
+    //保养移除
+    byyc(index,row){
+        console.log(index);
+        this.tableData.splice(index,1);
+        console.log(this.tableData);
+        //this.bytable.splice(index,1);
+        this.$nextTick(() => {
+        this.$refs.dataTable.toggleRowSelection(row,false);
+      });
+        
+    },
+    //保养项目确认
+    byqr(){
+      this.tableData=this.bytable;
+      this.dialogVisible1=false;
+      console.log(this.tableData);
+    },
+    // 选中的保养项目
+    handleSelectionChange(val) {
+      //console.log(val);
+      //console.log(val.length);
+      this.bynum = val.length;
+      this.bytable=val;
+    },
+    //分页  保养
+    handleSizeChange1(val) {
+      // console.log(`每页 ${val} 条`);
+      this.size1 = val;
+      this.chaxunbaoyang(this.size1, this.currentPage1);
+    },
+    handleCurrentChange1(val) {
+      // console.log(`当前页: ${val}`);
+      this.currentPage1 = val;
+      this.chaxunbaoyang(this.size1, this.currentPage1);
+    },
+    //关闭
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {});
+    },
+    //查询客户车辆
+    car(xm) {
+      let that = this;
+      axios
+        .post("http://127.0.0.1:8080/dzw_sys/api/lt/clicar/" + xm)
+        .then(function (res) {
+          // console.log(res.data);
+          that.caroptions = res.data;
+          that.inststion.cno = res.data[0].cno;
+        });
+    },
+    //查询用户
+    chaxunclient() {
+      let that = this;
+      axios
+        .post("http://127.0.0.1:8080/dzw_sys/api/lt/cli/1/100/undefined")
+        .then(function (res) {
+          //console.log(res.data.list);
+          that.clioptions = res.data.list;
+          that.xm = res.data.list[0].cliphone;
+          that.car(that.xm); //查询用户车辆
+        });
+    },
+    //保养项目
+    chaxunbaoyang(size1, currentPage1) {
+      let that = this;
+      axios
+        .get(
+          "http://127.0.0.1:8080/dzw_sys/api/tzy/shop/queryshopby/null/" +
+            currentPage1 +
+            "/" +
+            size1
+        )
+        .then(function (res) {
+          that.bytableData = res.data.list;
+          // console.log(res.data.list);
+          that.total1 = res.data.total;
+        });
+    },
+    //查询维修
+    chaxunweix() {
+      let that = this;
+      axios
+        .get(
+          "http://127.0.0.1:8080/dzw_sys/api/tzy/shop/queryAA/" + 1 + "/" + 100
+        )
+        .then(function (res) {
+          //console.log(res.data.list);
+        });
+    },
+    //查询救援车辆
+    chaxunWorkcar() {
+      let that = this;
+      axios
+        .post(
+          "http://127.0.0.1:8080/dzw_sys/api/Workcars/All/" + 100 + "/" + 1,
+          this.workcar
+        )
+        .then(function (res) {
+          //console.log(res.data.list);
+          that.workcaroptions = res.data.list;
+          if (res.data.list.length != 0) {
+            that.inststion.wid = res.data.list[0].wid;
+          }
+        });
+    },
+    //查询班组
+    chaxunTeam() {
+      const axios = require("axios");
+      let that = this;
+      axios.get("http://localhost:8080/dzw_sys/api/Teams").then(function (res) {
+        //console.log(res.data);
+        that.teamoptions = res.data;
+        if (res.data.length != 0) {
+          that.inststion.tid = res.data[0].tid;
+        }
+      });
+    },
+    //返回
+    goBack() {},
     //百度地图
     bddt() {
       this.map = new BMap.Map("container");
@@ -231,7 +462,14 @@ export default {
       this.map.addControl(new BMap.OverviewMapControl()); //添加缩略地图控件
       this.localSearch = new BMap.LocalSearch(this.map);
     },
+    //点击查询地址
     chaxdt() {
+      if (this.zd == "") {
+        this.$message({
+          message: "请输入接车地址！",
+          type: "warning",
+        });
+      }
       var keyword = this.zd;
       let that = this;
       this.localSearch.setSearchCompleteCallback(function (searchResult) {
@@ -260,6 +498,7 @@ export default {
           that.map.addOverlay(lab3);
           var a = BMapLib.GeoUtils.getDistance(myP1, myP3);
           console.log("距离：" + a);
+          that.inststion.lc = (a / 1000).toFixed(1);
           setTimeout(function () {
             that.map.setViewport([myP1, myP3]); //调整到最佳视野
           }, 1000);
@@ -277,5 +516,13 @@ export default {
 }
 .dt {
   margin-bottom: 20px;
+}
+.el-page-header {
+  margin-bottom: 20px;
+}
+.el-radio-group {
+  position: absolute;
+  right: 2%;
+  top: 1%;
 }
 </style>
